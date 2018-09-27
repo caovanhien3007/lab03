8bf0d802fcdb8ad77b0bc760fb96ba7938aa8e17
cao hien <johndoe@example.com>
2018-09-27 22:09:46 +0700

    submit lab03

---
+++ causo3/src/causo3/Causo3.java
@@ -0,0 +1,368 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package causo3;
+
+/**
+ *
+ * @author user56
+ */
+public class Causo3 {
+public class hocsinh{
+    String ten,lop;
+    int tuoi,ngaysinh;
+
+        public String getTen() {
+            return ten;
+        }
+
+        public void setTen(String ten) {
+            this.ten = ten;
+        }
+
+        public String getLop() {
+            return lop;
+        }
+
+        public void setLop(String lop) {
+            this.lop = lop;
+        }
+
+        public int getTuoi() {
+            return tuoi;
+        }
+
+        public void setTuoi(int tuoi) {
+            this.tuoi = tuoi;
+        }
+
+        public int getNgaysinh() {
+            return ngaysinh;
+        }
+
+        public void setNgaysinh(int ngaysinh) {
+            this.ngaysinh = ngaysinh;
+        }
+    
+} 
+public class sinhvien{
+    String ten,khoahoc;
+    int mssv,ngaysinh;
+
+        public String getTen() {
+            return ten;
+        }
+
+        public void setTen(String ten) {
+            this.ten = ten;
+        }
+
+        public String getKhoahoc() {
+            return khoahoc;
+        }
+
+        public void setKhoahoc(String khoahoc) {
+            this.khoahoc = khoahoc;
+        }
+
+        public int getMssv() {
+            return mssv;
+        }
+
+        public void setMssv(int mssv) {
+            this.mssv = mssv;
+        }
+
+        public int getNgaysinh() {
+            return ngaysinh;
+        }
+
+        public void setNgaysinh(int ngaysinh) {
+            this.ngaysinh = ngaysinh;
+        }
+    
+}
+public class giaovien{
+    String ten,mon;
+    int tuoi,ngaysinh;
+
+        public String getTen() {
+            return ten;
+        }
+
+        public void setTen(String ten) {
+            this.ten = ten;
+        }
+
+        public String getMon() {
+            return mon;
+        }
+
+        public void setMon(String mon) {
+            this.mon = mon;
+        }
+
+        public int getTuoi() {
+            return tuoi;
+        }
+
+        public void setTuoi(int tuoi) {
+            this.tuoi = tuoi;
+        }
+
+        public int getNgaysinh() {
+            return ngaysinh;
+        }
+
+        public void setNgaysinh(int ngaysinh) {
+            this.ngaysinh = ngaysinh;
+        }
+    }
+public class tongthong{
+    String ten,nhiemki;
+    int ngaysinh;
+
+        public String getTen() {
+            return ten;
+        }
+
+        public void setTen(String ten) {
+            this.ten = ten;
+        }
+
+        public String getNhiemki() {
+            return nhiemki;
+        }
+
+        public void setNhiemki(String nhiemki) {
+            this.nhiemki = nhiemki;
+        }
+
+        public int getNgaysinh() {
+            return ngaysinh;
+        }
+
+        public void setNgaysinh(int ngaysinh) {
+            this.ngaysinh = ngaysinh;
+        }
+    
+}
+public class concho{
+    String ten,gioitinh,cuaai;
+    int tuoi;
+
+        public String getTen() {
+            return ten;
+        }
+
+        public void setTen(String ten) {
+            this.ten = ten;
+        }
+
+        public String getGioitinh() {
+            return gioitinh;
+        }
+
+        public void setGioitinh(String gioitinh) {
+            this.gioitinh = gioitinh;
+        }
+
+        public String getCuaai() {
+            return cuaai;
+        }
+
+        public void setCuaai(String cuaai) {
+            this.cuaai = cuaai;
+        }
+
+        public int getTuoi() {
+            return tuoi;
+        }
+
+        public void setTuoi(int tuoi) {
+            this.tuoi = tuoi;
+        }
+    }
+public class caiban{
+    String loai;
+    int gia,chatluong,cannang;
+
+        public String getLoai() {
+            return loai;
+        }
+
+        public void setLoai(String loai) {
+            this.loai = loai;
+        }
+
+        public int getGia() {
+            return gia;
+        }
+
+        public void setGia(int gia) {
+            this.gia = gia;
+        }
+
+        public int getChatluong() {
+            return chatluong;
+        }
+
+        public void setChatluong(int chatluong) {
+            this.chatluong = chatluong;
+        }
+
+        public int getCannang() {
+            return cannang;
+        }
+
+        public void setCannang(int cannang) {
+            this.cannang = cannang;
+        }
+    
+}
+public class caighe{
+    String loaighe;
+    int gia,soluong;
+
+        public String getLoaighe() {
+            return loaighe;
+        }
+
+        public void setLoaighe(String loaighe) {
+            this.loaighe = loaighe;
+        }
+
+        public int getGia() {
+            return gia;
+        }
+
+        public void setGia(int gia) {
+            this.gia = gia;
+        }
+
+        public int getSoluong() {
+            return soluong;
+        }
+
+        public void setSoluong(int soluong) {
+            this.soluong = soluong;
+        }
+    }
+public class oto{
+    String ten,mau,loai;
+    int ms,gia;
+
+        public String getTen() {
+            return ten;
+        }
+
+        public void setTen(String ten) {
+            this.ten = ten;
+        }
+
+        public String getMau() {
+            return mau;
+        }
+
+        public void setMau(String mau) {
+            this.mau = mau;
+        }
+
+        public String getLoai() {
+            return loai;
+        }
+
+        public void setLoai(String loai) {
+            this.loai = loai;
+        }
+
+        public int getMs() {
+            return ms;
+        }
+
+        public void setMs(int ms) {
+            this.ms = ms;
+        }
+
+        public int getGia() {
+            return gia;
+        }
+
+        public void setGia(int gia) {
+            this.gia = gia;
+        }
+    }
+public class ngoinha{
+    String diachi,chunha;
+    int sonha,sophong;
+
+        public String getDiachi() {
+            return diachi;
+        }
+
+        public void setDiachi(String diachi) {
+            this.diachi = diachi;
+        }
+
+        public String getChunha() {
+            return chunha;
+        }
+
+        public void setChunha(String chunha) {
+            this.chunha = chunha;
+        }
+
+        public int getSonha() {
+            return sonha;
+        }
+
+        public void setSonha(int sonha) {
+            this.sonha = sonha;
+        }
+
+        public int getSophong() {
+            return sophong;
+        }
+
+        public void setSophong(int sophong) {
+            this.sophong = sophong;
+        }
+    }
+public class truonghoc{
+    String tentruong,diachi;
+    int ngaythanhlap;
+
+        public String getTentruong() {
+            return tentruong;
+        }
+
+        public void setTentruong(String tentruong) {
+            this.tentruong = tentruong;
+        }
+
+        public String getDiachi() {
+            return diachi;
+        }
+
+        public void setDiachi(String diachi) {
+            this.diachi = diachi;
+        }
+
+        public int getNgaythanhlap() {
+            return ngaythanhlap;
+        }
+
+        public void setNgaythanhlap(int ngaythanhlap) {
+            this.ngaythanhlap = ngaythanhlap;
+        }
+    }
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
